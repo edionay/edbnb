7e81549d611c55fad1d04ee732755654cc27b9a6
Edionay Aguiar <edionay@gmail.com>
2019-10-06 23:49:35 -0300

    Updates

---
+++ precache-manifest.f1a7bb9ebc283c35917dec15b7261a5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a75b4cd0da82a1de400ce3f73b5186a0",
+    "url": "/edbnb/index.html"
+  },
+  {
+    "revision": "7d5ecc1e9900150ded55",
+    "url": "/edbnb/static/css/main.a9c0be35.chunk.css"
+  },
+  {
+    "revision": "ae5b96c7bd455f2dd66e",
+    "url": "/edbnb/static/js/2.86472728.chunk.js"
+  },
+  {
+    "revision": "7d5ecc1e9900150ded55",
+    "url": "/edbnb/static/js/main.e75af569.chunk.js"
+  },
+  {
+    "revision": "d160d160f6e458cbb2e0",
+    "url": "/edbnb/static/js/runtime-main.36179d12.js"
+  },
+  {
+    "revision": "83252e0afaf5f134063345abd6ac8fd7",
+    "url": "/edbnb/static/media/background.83252e0a.jpg"
+  },
+  {
+    "revision": "6a6f4b6ebebfabdc283a3a6b417282a0",
+    "url": "/edbnb/static/media/camera.6a6f4b6e.svg"
+  },
+  {
+    "revision": "8f15ca1b933f38c3a735a089a25c4b69",
+    "url": "/edbnb/static/media/logo.8f15ca1b.svg"
+  }
+]);
